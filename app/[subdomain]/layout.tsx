d1e03b1d39573561f641c74a9aa410d6850bb961
Andrio <andrio.rodrigo.silva@hotmail.com>
2024-07-24 19:29:08 -0300

    add home page

---
+++ app/[subdomain]/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "react-toastify/dist/ReactToastify.css";
+import "@/styles/custom.css";
+import "@/styles/global.css";
+import "@/app/globals.css";
+import "@/app/theme-config.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Startups Global Link",
+  description: "Business platform for Startups with Artificial Intelligence",
+  icons: {
+    icon: "/favicon.svg",
+  },
+};
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="min-h-screen">{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ app/[subdomain]/page.tsx
@@ -0,0 +1,5 @@
+import HomeComponent from "@/components/external/home/home";
+
+export default function Home() {
+  return <HomeComponent />;
+}
