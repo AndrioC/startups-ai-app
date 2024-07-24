0d61951364cd667f4c7b1fbcd3c47a8a9a977359
Andrio <andrio.rodrigo.silva@hotmail.com>
2024-07-24 19:34:29 -0300

    remove console.log

--- app/[subdomain]/(auth-routes)/auth/login/page.tsx
+++ app/[subdomain]/(auth-routes)/auth/login/page.tsx
@@ -1,14 +1,5 @@
-"use client";
-
-import { useParams, usePathname } from "next/navigation";
-
 import LoginComponent from "@/components/external/login/login-page";
 
 export default function Login() {
-  const teste = useParams();
-  const teste2 = usePathname();
-
-  console.log("teste", teste);
-  console.log("teste2", teste2);
   return <LoginComponent />;
 }
