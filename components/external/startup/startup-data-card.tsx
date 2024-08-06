0cfe9d9edfd7b51a658558b540669ab1c120e742
Andrio <andrio.rodrigo.silva@hotmail.com>
2024-08-06 13:31:42 -0300

    fix sidebar show data

--- components/external/startup/startup-data-card.tsx
+++ components/external/startup/startup-data-card.tsx
@@ -15,10 +15,10 @@ interface Props {
 
 export default function StartupDataCard({ data }: Props) {
   return (
-    <div className="w-[300px] h-[600px] bg-[#F1F3F3] shadow-md rounded-lg text-gray-500">
-      <div className="text-xl font-bold mb-4 pl-5 mt-5">DADOS DA STARTUP</div>
-      <div className="flex justify-between pl-5 pr-5 mb-5 w-[270px] gap-2">
-        <div className="logo-container">
+    <div className="w-[300px] bg-[#F1F3F3] shadow-md rounded-lg text-gray-500 flex flex-col">
+      <div className="text-xl font-bold p-5 pb-4">DADOS DA STARTUP</div>
+      <div className="flex justify-between px-5 mb-5 w-full">
+        <div className="logo-container w-[100px] h-[100px] relative">
           <Image
             src={data.loadLogoUrl || placeholder}
             alt="Startup Logo"
@@ -28,38 +28,42 @@ export default function StartupDataCard({ data }: Props) {
         </div>
         <Image src={brazilFlag} alt="Brazil Flag" className="w-8 h-8" />
       </div>
-      <ul className="space-y-3 mb-4 font-medium pl-5 mt-3">
+      <ul className="space-y-3 mb-4 font-medium px-5">
         <li className="flex items-center gap-1">
           <HiOutlineBuildingOffice2 size={18} />
-          {data?.startupName ? data?.startupName : "-"}
+          <span className="break-words">{data?.startupName || "-"}</span>
         </li>
         <li className="flex items-center gap-1">
           <FaLocationDot size={18} />
-          {data?.stateAndCity ? data?.stateAndCity : "-"}
+          <span className="break-words">{data?.stateAndCity || "-"}</span>
         </li>
         <li className="flex items-center gap-1">
           <RiFolderLockLine size={18} />
-          {data?.businessModelText ? data?.businessModelText : "-"}
+          <span className="break-words">{data?.businessModelText || "-"}</span>
         </li>
-        <li className="flex gap-1">
-          <MdOutlineAddBusiness size={18} className="mt-0.5" />
-          {data?.verticalText ? data?.verticalText : "-"}
+        <li className="flex items-start gap-1">
+          <MdOutlineAddBusiness size={18} className="mt-1" />
+          <span className="break-words">{data?.verticalText || "-"}</span>
         </li>
       </ul>
       <hr className="border-gray-300 mb-4 w-[260px] mx-auto" />
-      <div className="font-bold mb-4 pl-5">DADOS DO RESPONSÁVEL</div>
-      <ul className="space-y-3 mb-4 font-medium pl-5 mt-3">
+      <div className="font-bold mb-4 px-5">DADOS DO RESPONSÁVEL</div>
+      <ul className="space-y-3 mb-4 font-medium px-5">
         <li className="flex items-center gap-1">
           <FaRegUser size={18} />
-          {data?.mainResponsibleName ? data?.mainResponsibleName : "-"}
+          <span className="break-words">
+            {data?.mainResponsibleName || "-"}
+          </span>
         </li>
         <li className="flex items-center gap-1">
           <MdOutlineMailOutline size={18} />
-          {data?.mainResponsibleEmail ? data?.mainResponsibleEmail : "-"}
+          <span className="break-words">
+            {data?.mainResponsibleEmail || "-"}
+          </span>
         </li>
         <li className="flex items-center gap-1">
           <FiPhone size={18} />
-          {data?.contactNumber ? data?.contactNumber : "-"}
+          <span className="break-words">{data?.contactNumber || "-"}</span>
         </li>
       </ul>
     </div>
