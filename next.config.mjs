eb25c55ab67cae3260d40a006a2ab751d74b4ebe
Andrio <andrio.rodrigo.silva@hotmail.com>
2024-09-08 16:51:14 -0300

    add company page

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,17 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "organizations-imgs.s3.amazonaws.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "user-sgl-imgs.s3.amazonaws.com",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "startups-logo-imgs.s3.amazonaws.com",
+        pathname: "/**",
       },
     ],
   },
