d021df3e065f616a8b62684a210236aec3924aea
Andrio <andrio.rodrigo.silva@hotmail.com>
2024-11-11 18:53:19 -0300

    remove is_approved condition from dashboard data

--- app/api/dashboard/[organization_id]/dash-data/route.ts
+++ app/api/dashboard/[organization_id]/dash-data/route.ts
@@ -49,7 +49,6 @@ export async function GET(
       where: {
         organization_id: orgId,
         is_deleted: false,
-        is_approved: true,
       },
       select: { created_at: true, country_id: true },
     });
