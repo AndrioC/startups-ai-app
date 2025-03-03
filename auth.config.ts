155aa2612ca8f254becad659e4d98b3dc8d38241
Andrio <andrio.rodrigo.silva@hotmail.com>
2025-03-02 23:51:54 -0300

    testing google login

--- auth.config.ts
+++ auth.config.ts
@@ -3,6 +3,7 @@ import bcryptjs from "bcryptjs";
 import { cookies } from "next/headers";
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import Google from "next-auth/providers/google";
 import { createTranslator } from "next-intl";
 
 import { getUserByEmail } from "@/data/user";
@@ -22,6 +23,18 @@ async function getServerTranslations() {
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
     Credentials({
       async authorize(credentials) {
         try {
